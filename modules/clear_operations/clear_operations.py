8ada66920d3ef40c098037aeb0d67e3a72ab30ce
yegorkowalew <kowalew.backup@gmail.com>
2018-08-29 00:01:19 +0300

    Вынес clear_operations в отдельный модуль

---
+++ modules/clear_operations/clear_operations.py
@@ -0,0 +1,8 @@
+from settings import operations_names
+
+def say_hallo(cell, operations_names):
+    print('hallo', ' ', cell)
+
+if __name__ == '__main__':
+    say_hallo('my name', operations_names)
+    print(operations_names)
