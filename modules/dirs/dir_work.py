c48005422fe6f836684bc320999413b3478eea04
Коваленко Ігор Сергійович <i.kovalenko@kmzindustries.ua>
2018-08-30 11:42:35 +0300

    Исправил баг когда файлы брались не по пути выполнения основного файла а по пути подключенного модуля.

--- modules/dirs/dir_work.py
+++ modules/dirs/dir_work.py
@@ -1,9 +1,9 @@
-import os
+import os,sys
 import inspect
 import time
 
 # select = {0: "Все"}
-
+# 
 def work_with_dir():
     folder_for_find_xlsx = os.path.dirname(os.path.abspath(inspect.stack()[0][1])) # полный путь к папке из которой выполняется файл
     list_files = [] # список файлов с полными путями
@@ -13,7 +13,9 @@ def work_with_dir():
     return list_files
 
 def list_dir():
-    folder_for_find_xlsx = os.path.dirname(os.path.abspath(inspect.stack()[0][1])) # полный путь к папке из которой выполняется файл
+    # print(os.path.dirname(os.path.realpath(sys.argv[0])))
+    # folder_for_find_xlsx = os.path.dirname(os.path.abspath(inspect.stack()[0][1])) # полный путь к папке из которой выполняется файл
+    folder_for_find_xlsx = os.path.dirname(os.path.realpath(sys.argv[0])) # полный путь к папке из которой выполняется файл
     list_dir = [] # список файлов с полными путями
     for file in os.listdir(folder_for_find_xlsx): # во всех файлах папки
             if file.endswith(".xlsx"): # находим файл совпадающий с шаблоном
@@ -41,6 +43,10 @@ def check_input(files_lst, user_input):
     return list(files_list), list(failed)
 
 def user_input_work():
+    # print(os.path.realpath(__file__))
+    # print(os.path.basename(os.path.realpath(sys.argv[0])))
+    
+    # print(os.path.dirname(os.path.abspath(__file__)))
     print('Какие файлы обрабатывать?')
     i = 1
     for val in list_dir():
@@ -51,7 +57,10 @@ def user_input_work():
     for_work = check_input(list_dir(), user_input)
     if for_work[1] == []:
         print('Обрабатываю файлы:\n','\n '.join(for_work[0]))
-        return for_work[0]
+        # print(os.path.dirname(os.path.realpath(sys.argv[0])))
+        # new_list = 
+        # print(new_list)
+        return [os.path.dirname(os.path.realpath(sys.argv[0]))+'\\'+x for x in for_work[0]]
     else:
         if for_work[0] == []:
             print('Обрабатывать нечего.\n Неверный ввод: ', ', '.join(for_work[1]))
